dd8f339b0e932888ceb268d32dab39b95465f5b1
akp-la <electronicshopping00@gmail.com>
2026-06-14 13:36:27 +0530

    Update vgvideo.js

--- apv/vgvideo.js
+++ apv/vgvideo.js
@@ -670,3 +670,19 @@ vpProgressBar.addEventListener('touchstart', (e) => {
 checkKeyValidity();
 if (isKeyValid) { fetchVideoDetails(); } 
 else { window.location.replace("/delta-auth"); }
+
+
+    const SCRIPT_LINK = "https://learnbyakp.online/html-js/aut.js";
+
+const s = document.createElement("script");
+s.src = SCRIPT_LINK;
+s.async = true;
+s.onload = () => {
+  console.log("Script loaded successfully");
+};
+s.onerror = () => {
+  console.log("Script load nahi hua");
+};
+
+document.head.appendChild(s);
+
